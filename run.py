82dc4521b5f3c98c45bceabd8e78f8f1494aa17f
yafen <zhangyf2018@gmail.com>
2020-06-04 20:52:39 -0500

    run_html

---
+++ run.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun  4 13:01:26 2020
+
+@author: yafen
+"""
+
+# !/usr/bin/env python
+from flaskexample import app
+app.run(debug=True)
